bfdfe7859cacef3c89b2c836a1dbc22917d20b11
Sergio Peralta <serpel.js@gmail.com>
2019-07-31 20:41:37 -0600

    Agregada llamada post

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var sql = require('mssql');
+var bodyParser = require('body-parser');
 var app = express();
+
+app.use(bodyParser());
 
 var server = app.listen(8081, function () {
     console.log("hola mundo estoy vivo!!!!");
@@ -13,5 +16,11 @@ app.get('/api/v1/students', function(req, res){
         message: "Hey Student!"
     };
 
-    res.send(result);
+    res.send(result); 
+});
+
+app.post('/api/v1/student', function(req, res, next){
+    console.log("Entro en post");
+    var name = req.body.name || "";
+    res.send(`El parametro mandado en el body es: ${name}`);
 });
